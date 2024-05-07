55679fe1a11f5e56bab2248a8af40a2744934589
PatrickJS <github@patrickjs.com>
2024-05-07 13:41:19 -0700

    "moduleResolution": "bundler",

--- src/root.tsx
+++ src/root.tsx
@@ -5,7 +5,7 @@ import {
   ServiceWorkerRegister,
 } from "@builder.io/qwik-city";
 
-import { RouterHead } from "./components/router-head/router-head.tsx";
+import { RouterHead } from "./components/router-head/router-head.js";
 // import { RouterHead } from "./components/router-head/router-head.js";
 
 import "./global.css";
